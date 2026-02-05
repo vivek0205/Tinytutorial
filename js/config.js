ae79ed22ba453672fab79039f376b2137aaaba7d
vivek0205 <vivek.co@gmail.com>
2026-02-05 12:56:05 +0530

    Update Cognito configuration in config.js
    Updated Cognito user pool ID and client ID in config.

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_CxwfmVV6o', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7hqkvhir76sue3fbhe259k09cf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_CUoJaaz80', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1osojssrfj8gnsj2fh8g3i99lc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://ah93yq8y22.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
